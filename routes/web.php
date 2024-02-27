993d5ea1bb3341b1d34a6ef1cb0cd1b781444f41
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-02-27 13:11:17 +0500

    webfiles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//BACKEND
+Route::get('/dashboard',[DashboardController::class, 'index'])->name('home');
+
+
+//Frontend
+Route::get('/',[HomeController::class, 'index'])->name('home');
