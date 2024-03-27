016c23396b6764f1d99bc40a4209eee167e0baf6
UmairRathore <contactusr22@gmail.com>
2024-03-28 00:01:16 +0500

    web portal updated

---
+++ database/migrations/2024_03_27_123843_create_user_badges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBadgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_badges', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('badge_id')->nullable();
+            $table->timestamps();
+            $table->foreign('badge_id')->references('id')->on('badge')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_badges');
+    }
+}
